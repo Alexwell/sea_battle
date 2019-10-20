ddc3465d10ecb69b0d59ffe817bd9efaa511be7a
Alexwell <alexwebq@gmail.com>
2019-10-20 19:18:12 +0300

    Ship setting

--- lib/sea_battle.rb
+++ lib/sea_battle.rb
@@ -9,27 +9,31 @@ def main
   # f.create_game_field
   # p f.coordinates
   # f.coordinates.each{|i| puts "#{i[0]} <==> #{i[0].next}"}
-  s4_1 = Ship.new(4, true)
-  s3_1 = Ship.new(3, false)
-  s3_2 = Ship.new(3, false)
-  s2_1 = Ship.new(2, true)
 
-  s2_2 = Ship.new(4, true)
 
-  s2_3 = Ship.new(2, false)
-  s1_1 = Ship.new(1, true)
-  s1_2 = Ship.new(1, true)
-  s1_3 = Ship.new(1, false)
-  s1_4 = Ship.new(1, false)
+  s4_0 = Ship.new(4, false)
+  s3_1_0 = Ship.new(3, false)
+  s3_2_1 = Ship.new(3, true)
+  s2_1_0 = Ship.new(2, false)
+  s2_2_1 = Ship.new(2, true)
+  s2_3_1 = Ship.new(2, true)
+  s1_1_0 = Ship.new(1, false)
+  s1_2_0 = Ship.new(1, false)
+  s1_3_1 = Ship.new(1, true)
+  s1_4_1 = Ship.new(1, true)
+
+
+  f.ship_set(s4_0, 1, 10)
+  f.ship_set(s3_1_0, 2, 8)
+  f.ship_set(s3_2_1, 1, 4)
+  f.ship_set(s2_1_0, 9, 1)
+  f.ship_set(s2_2_1, 10, 9)
+  f.ship_set(s2_3_1, 1, 1)
+  f.ship_set(s1_1_0, 6, 8)
+  f.ship_set(s1_2_0, 5, 4)
+  f.ship_set(s1_3_1, 6, 6)
+  f.ship_set(s1_4_1, 7, 1)
   
-  # p f.coordinates
-
-  # f.set_ship(s3_2, 5, 2)
-  # f.set_ship(s3_1, 0, 0)
-  # f.set_ship(s3_1, 2, 0)
-  # f.set_ship(s3_1, 5, 1)
-  # f.set_ship(s3_1, 3, 7)
-  f.ship_set(s2_2, 5, 6)
   f.print_game_field
   p f.coordinates
 
--- lib/sea_battle/GameField.rb
+++ lib/sea_battle/GameField.rb
@@ -2,25 +2,27 @@ class GameField
   @@cell_default = " ~ "
   @@cell_shooted = " + "
   @@cell_ship = "[ ]"
-  @@cell_border = " $ "
+  @@cell_border = " - "
   @@cell_damaged = " x "
 
   attr_reader :coordinates
 
   def initialize(size)
-    @size = size
+    @size = size + 1
     @coordinates = create_game_field
   end
 
   def print_game_field
     print " "
-    (0...@size).each { |i| print " #{i} " }
+    print " " if @size > 9
+    (1...@size).each { |i| print " #{i} " }
     puts
-    i = 0
+    i = 1
     while i < @size
       print i
+      print " " unless i == @size - 1
       j = 0
-      while j< @size
+      while j< @size - 1
         print @coordinates[i + @size*j][2]
         # print " #{@coordinates[i+@size*j][0]} <=> #{@coordinates[i+@size*j][1]} "
         j += 1
@@ -34,6 +36,7 @@ class GameField
 
 
   def ship_set(ship, x, y)
+    x -= 1
     if !ship_check(ship, x, y)
       return
     end
@@ -44,18 +47,30 @@ class GameField
         while j < ship.size
           if ship.vertical
             @coordinates[i + j][2] = @@cell_ship
-            
+
             @coordinates[i - 1][2] = @@cell_border
             @coordinates[i - 1 - @size][2] = @@cell_border
             @coordinates[i + ship.size - @size][2] = @@cell_border
             @coordinates[i + j - @size][2] = @@cell_border
 
             @coordinates[i + ship.size][2] = @@cell_border
-            @coordinates[i - 1 + @size][2] = @@cell_border 
-            @coordinates[i + j + @size][2] = @@cell_border 
-            @coordinates[i  + ship.size + @size][2] = @@cell_border
+            @coordinates[i - 1 + @size][2] = @@cell_border
+            @coordinates[i + j + @size][2] = @@cell_border
+            @coordinates[i  + ship.size + @size][2] = @@cell_border unless @coordinates[i  + ship.size + @size] == nil
           else
             @coordinates[i + @size*j][2] = @@cell_ship
+
+            @coordinates[i - 1 - @size][2] = @@cell_border
+            @coordinates[i - @size][2] = @@cell_border
+            @coordinates[i - 1 + @size*ship.size][2] = @@cell_border unless @coordinates[i - 1 + @size*ship.size] == nil
+            @coordinates[i - 1 + @size * j][2] = @@cell_border
+
+            @coordinates[i + 1 - @size][2] = @@cell_border
+            @coordinates[i + @size*ship.size][2] = @@cell_border unless @coordinates[i + @size*ship.size] == nil
+            @coordinates[i + 1 + @size*ship.size][2] = @@cell_border unless @coordinates[i + 1 + @size*ship.size] == nil
+            @coordinates[i + 1 + @size * j][2] = @@cell_border
+
+
 
           end
           j += 1
@@ -86,19 +101,35 @@ class GameField
 
 
   def ship_check(ship, x, y)
-    sum_x = x + ship.size
-    sun_y = y + ship.size
-    if sum_x > @size or sun_y > @size
-      p "Wrong coordinate, ship is too big!"
-      return false
+    if ship.vertical
+      if y  + ship.size > @size
+        p "Wrong coordinate 'y', ship is too big!"
+        return false
+      end
+    else
+      if x  + ship.size >= @size
+        p "Wrong coordinate 'x', ship is too big!"
+        return false
+      end
     end
-
     i = 0
     while i < @coordinates.length
-      if @coordinates[i][0] == x and @coordinates[i][1] == y \
-          and @coordinates[i][2] != @@cell_default
-        p "Cell is already filled"
-        return false
+      if @coordinates[i][0] == x and @coordinates[i][1] == y
+        j = 0
+        while j < ship.size
+          if ship.vertical
+            if  @coordinates[i + j][2] != @@cell_default
+               p "Cell is already filled, vertical"
+              return false
+            end
+          else
+            if @coordinates[i + @size * j][2] != @@cell_default
+               p "Cell is already filled, horizontal"
+              return false
+            end
+          end
+          j += 1
+        end
       end
       i += 1
     end
